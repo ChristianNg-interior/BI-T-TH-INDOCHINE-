dab25b778c91df6a7df9ff2ed901c82825f2faad
ChristianNg-interior <namhoangqlhau97@gmail.com>
2026-02-25 21:42:54 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,211 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-phng-n",
+      "name": "PHÒNG ĂN",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0245811136135714,
+          "pitch": 0.11728858669390263,
+          "rotation": 0,
+          "target": "1-phng-bp"
+        },
+        {
+          "yaw": 0.46421803075828016,
+          "pitch": 0.23809815389811284,
+          "rotation": 0,
+          "target": "3-snh-v-phng-th"
+        },
+        {
+          "yaw": 0.9572679697845281,
+          "pitch": 0.0328398752455179,
+          "rotation": 0,
+          "target": "2-phng-khch"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-phng-bp",
+      "name": "PHÒNG BẾP",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5573241765586161,
+          "pitch": 0.12865377891852603,
+          "rotation": 0,
+          "target": "0-phng-n"
+        },
+        {
+          "yaw": 0.1595515872534783,
+          "pitch": 0.06882131583830642,
+          "rotation": 0,
+          "target": "3-snh-v-phng-th"
+        },
+        {
+          "yaw": 0.42893163989960925,
+          "pitch": -0.02021320807398297,
+          "rotation": 0,
+          "target": "2-phng-khch"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-phng-khch",
+      "name": "PHÒNG KHÁCH",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.22918245703956153,
+          "pitch": 0.17978434672370547,
+          "rotation": 0,
+          "target": "3-snh-v-phng-th"
+        },
+        {
+          "yaw": -0.6603132018451063,
+          "pitch": 0.1163680665793887,
+          "rotation": 0,
+          "target": "0-phng-n"
+        },
+        {
+          "yaw": -0.704235653422991,
+          "pitch": -0.007531314505056841,
+          "rotation": 0,
+          "target": "1-phng-bp"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-snh-v-phng-th",
+      "name": "SẢNH VÀ PHÒNG THỜ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2470633814417926,
+          "pitch": 0.3134452937325243,
+          "rotation": 0,
+          "target": "0-phng-n"
+        },
+        {
+          "yaw": -1.382644306856637,
+          "pitch": 0.06482523039709065,
+          "rotation": 0,
+          "target": "1-phng-bp"
+        },
+        {
+          "yaw": 1.356501860498712,
+          "pitch": 0.006256372861527026,
+          "rotation": 0,
+          "target": "2-phng-khch"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
